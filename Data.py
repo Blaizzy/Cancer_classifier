8d5a43505f8070d195d140da6e0a47b37f7e3ac8
Prince Canuma <prince.gdt@gmail.com>
2019-02-03 17:16:10 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -9,19 +9,36 @@ import itertools
 class Dataset:
 
     '''This class is created and optimized by Prince Canuma for the  Wisconsin Breast Cancer Database (January 8, 1991)
-    citation:
+
+    Github profile:  https://github.com/Blaizzy
+    Medium profile:  https://medium.com/@prince.canuma
+
+
+    Citation:
      K. P. Bennett & O. L. Mangasarian: "Robust linear programming
      discrimination of two linearly inseparable sets", Optimization Methods
     and Software 1, 1992, 23-34 (Gordon & Breach Science Publishers
     ---------------- Methods ---------------------
-    load(): Gets the text in the .txt file, creates a pandas Dataframe, copies it to the class variable df \
-    and returns DataFrame.
+    load(): Gets the text in the .txt file, creates a pandas Dataframe(DF),
+    copies it to the class variable df and returns DataFrame(DF).
 
-    ---------------- Class Methods
-    scatterplot(): create a scatter plot with showing correlation and histogram of all columns
+    ---------------- Class Methods----------------------
+    scatter_plot(): uses the copied pandas DF and creates a scatter plot with showing correlation and histogram
+    of all columns.
 
-    df_scatter():
+    df_scatter_plot(*DataFrame as args): receives a pandas DF and creates a scatter plot with showing correlation a
+    nd histogram of all columns.
 
+    class_distribution(*Dataframe as args): receives a pandas df and plots a the label distribution
+
+    correlation_matrix(): Uses the class copy of the DF and displays a correlation matrix between attributes.
+
+    decode_preds(*predictions as args): recieves an array of predictions(0s or 1s)from the test set and returns the name of the
+    classes (Benign or Malignant)
+
+    confusion_matrix(*true_labels, *predictions): recieves two arguments, the first is an array of the true labels
+    and the second are the predicited label
+    ----------------------------------------------------------
 
     '''
     df = 0
@@ -36,14 +53,9 @@ class Dataset:
             str = i.strip('\n').split(',')
             text.append(str)
 
-        #for i in range(len(text[0])):
-         #   for j in range(len(text[0])):
-
-        #id= []
-        #for i in names:
-         #   id.append(i)
 
 
+        # Read each collumn into an 1D array
         id = [text[i][0] for i in range(len(text))]
         ct = [text[i][1] for i in range(len(text))]
         ucsize = [text[i][2] for i in range(len(text))]
@@ -56,13 +68,7 @@ class Dataset:
         mit = [text[i][9] for i in range(len(text))]
         label = [text[i][10] for i in range(len(text))]
 
-        #def columns(no_columns):
-            # as
-         #   for
-
-        #def dictionaries():
-            #dicts
-
+        # Create Dataframe
         df = pd.DataFrame({'Clump Thickness': ct,
                            'Uniformity of Cell Size': ucsize,
                            'Uniformity of Cell Shape': ucshape,
@@ -75,24 +81,36 @@ class Dataset:
                            'y': label,
                            }, index=id)
 
-
-        df = df.replace(to_replace='?', value=0)
+        # Attributes range is (1-10)
+        # Cleaning Missing values with the min value acceptable
+        # Both 0 and 1 don't affect accuracy
+        # from 2 to 10 it starts affecting accuracy
+        df = df.replace(to_replace='?', value=1)
+        # Standard Deviation & mean
         std = df["Bare Nuclei"].astype(int).std()
         mean = df["Bare Nuclei"].astype(int).mean()
         print('Standard deviation:',round(std,0),"\nMean:", round(mean,0))
-        df = df.replace(to_replace='?', value=int(round(std,0))) #has negligible difference in acc
+        # Replacing 0 by standard deviation
+        #df = df.replace(to_replace='?', value=int(round(std,0))) #has negligible difference in acc
+        print('\n-------------------statistical details---------------\n')
         print(df.describe())
+        print('\n-----------------sample of the Data-----------------')
         print(df.head())
-
-        Dataset.df =df
+        # copy data frame to class variable df.
+        Dataset.df = df.astype(int)
         return df
 
     @classmethod
     def scatter_plot(cls):
-        print(cls.df.head())
-
-        scatter_matrix(cls.df.loc[:, 'Clump Thickness':'Mitoses'], diagonal='kde')
-        # plt.tight_layout()
+        # collumn as numbers
+        df_ = cls.df.copy(deep =True)
+        new = ['ct', 'ucsize', 'ucshape', 'ma', 'secs', 'bn', 'bc', 'nn', 'mit', 'label']
+        old = 'Clump Thickness','Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size',\
+        'Bare Nuclei', 'Bland Chromatin','Normal Nucleoli', 'Mitoses','y'
+        df_.rename(columns = dict(zip(old, new)), inplace = True)
+        scatter_matrix(df_.loc[:, 'ct':'mit'], diagonal='kde')
+        plt.suptitle('Scatter Diagram')
+        plt.tight_layout()
         plt.show()
 
     @classmethod
@@ -107,40 +125,23 @@ class Dataset:
     def class_distribution(cls, df):
         sns.set(rc={'figure.figsize': (11.7, 8.27)})
         sns.distplot(df['y'], bins=2)
+        plt.title('Class Distribution')
         plt.show()
 
+
+
     @classmethod
-    def correlation_matrix(cls, df):
-        corr_matrix = df.corr().round(2)
+    def correlation_matrix(cls):
+        corr_matrix = cls.df.corr().round(2)
         sns.heatmap(data=corr_matrix, annot=True)
+        plt.suptitle('Correlation Matrix')
         plt.show()
 
-    @classmethod
-    def two_var_corr(cls, df):
-        # take a 5% sample as this is computationally expensive
-        df_sample = df.sample(frac=0.05)
-
-        # Pairwise plots
-        sns.pairplot(df_sample, hue="y")
-        plt.show()
-
-        plt.figure(figsize=(20, 5))
-
-        features = ['Uniformity of Cell Size', 'Mitoses']
-        target = df['y']
-
-        x = df[features[0]]
-        y = target
-        plt.scatter(x, y, marker='o')
-        plt.title(features[0])
-        plt.xlabel(features[0])
-        plt.ylabel('y')
-        plt.show()
 
     @classmethod
     def decode_preds(cls, pred):
         preds = list()
-        print('Decoded classes:')
+        print('Classes Decoded succefully')
         for i in pred:
 
             if i == 0:
@@ -152,7 +153,9 @@ class Dataset:
             else:
                 continue
         preds = np.array(preds)
-        print(np.reshape(preds,(1,210)))
+        print(preds[:5])
+        #print(np.reshape(preds,(preds.shape)))
+        return preds
 
     @classmethod
     def plot_confusion_matrix(cls,cm, classes,
